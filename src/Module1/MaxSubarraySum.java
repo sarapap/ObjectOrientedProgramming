492ce9aee9e9765f9b27b78c0a019872596027f0
sarapappila <sara.pappila@metropolia.fi>
2024-01-18 11:56:41 +0200

    Done tasks

--- src/Module1/MaxSubarraySum.java
+++ src/Module1/MaxSubarraySum.java
@@ -1,4 +1,21 @@
 package Module1;
 
+import java.util.Scanner;
+
 public class MaxSubarraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int [] integers = new int[size];
+
+        System.out.println("Enter the integers into the array: ");
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Enter integer %d: ", i+1 );
+            integers[i] = scanner.nextInt();
+
+
 }
+} }
