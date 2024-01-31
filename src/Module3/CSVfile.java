6a53fa64445fa02848fae4edc8eb2484e5ead570
sarapappila <sara.pappila@metropolia.fi>
2024-01-31 14:07:01 +0200

    Working on

--- src/Module3/CSVfile.java
+++ src/Module3/CSVfile.java
@@ -1,4 +1,15 @@
 package Module3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class CSVfile {
+    private final static String FILENAME = "file.csv";
+    public static void main(String[] args) {
+        long[] fibonacci = generateFibonacci(100);
+    }
+
+    private static long[] generateFibonacci(int number) {
+        long[] fibonacci = new long[number];
+    }
 }
