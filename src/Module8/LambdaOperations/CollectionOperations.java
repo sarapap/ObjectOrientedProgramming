053a2da2bd3052ef1656282a897bc853dccb7c21
sarapappila <sara.pappila@metropolia.fi>
2024-03-13 19:48:10 +0200

    Tasks finished

--- src/Module8/LambdaOperations/CollectionOperations.java
+++ src/Module8/LambdaOperations/CollectionOperations.java
@@ -8,8 +8,10 @@ import java.util.stream.Stream;
 public class CollectionOperations {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(10, 5, 8, 20, 15, 3, 12));
-        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
-        System.out.println("Sum of numbers: " + sum);
+        int[] sum = {0};
+
+        numbers.forEach(number -> sum[0] += number);
+        System.out.println("Sum of numbers: " + sum[0]);
 
         numbers.removeIf(number -> number % 2 == 0);
         System.out.println("Without even numbers: " + numbers);
--- src/Module8/NumberList.java
+++ src/Module8/NumberList.java
@@ -6,8 +6,8 @@ public class NumberList {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(10, 5, 8, 20, 15, 3, 12);
 
-        int sum = numbers.stream().filter(number -> number % 2 != 0)
-                .peek(number -> System.out.println("Pariton luku: " + number))
+        int sum = numbers.stream().filter(number -> number % 2 == 0)
+                .peek(number -> System.out.println("Parillinen luku: " + number))
                 .mapToInt(number -> number * 2)
                 .sum();
         System.out.println("\nSum: " + sum);
