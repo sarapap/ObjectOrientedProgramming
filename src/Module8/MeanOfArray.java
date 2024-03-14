ab7dda879c82ab8d5f840ec9c25108fa84126988
sarapappila <sara.pappila@metropolia.fi>
2024-03-14 12:24:36 +0200

    Tasks finished

--- src/Module8/MeanOfArray.java
+++ src/Module8/MeanOfArray.java
@@ -1,13 +1,14 @@
 package Module8;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class MeanOfArray {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(10, 5, 8, 20, 15, 3, 12);
+        int[] numbers = {10, 5, 8, 20, 15, 3, 12};
 
-        double mean = numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+        double mean = Arrays.stream(numbers)
+                .average()
+                .orElse(0);
         System.out.println("Mean of numbers: " + mean);
     }
 }
